50bfcdaf10f5a72ebaa7bb53645a563bda1a2c89
Nobita <aurelio.ogliari@gmail.com>
2014-11-15 13:22:53 +0000

    move code inside a closure and expose the method

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,11 +2,17 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
+    pkg: grunt.file.readJSON('package.json'),
+    jasmine: {
+      src: ['dotdot.js'],
+      options: {
+        specs: 'test/*.spec.js',
+      }
+    }
   });
 
   // Load the plugin that provides the "uglify" task.
-  //grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
 
   // Default task(s).
   grunt.registerTask('default', []);
