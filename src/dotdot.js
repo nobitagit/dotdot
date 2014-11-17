bad7f7548860f64354fe8c8b2d21f9168bb677b2
Nobita <aurelio.ogliari@gmail.com>
2014-11-17 15:58:43 +0000

    account for nodelists to be passed

--- src/dotdot.js
+++ src/dotdot.js
@@ -2,9 +2,8 @@
 
   'use strict';
 
-  var DEFAULTS = {
-    paste: false
-  };
+  var DEFAULTS = {},
+      arrowKeys = [37, 38, 39, 40];
   
   function _extend( a, b ) {
     for ( var prop in b ) {
@@ -15,31 +14,48 @@
     return a;
   }
 
-  var Dotdot = function( el, opts ){
+  function _attachEvts( elem ){
+    elem.addEventListener('keyup', _addDots, false); 
+    elem.addEventListener('paste', _addDots, false); 
+  }
+
+  function _addDots(e) {
+    // Don't execute anything if the user is just moving the cursor around and
+    // not really enetring anything new.
+    if(arrowKeys.indexOf(e.keyCode) !== -1){ return; }
+
+    // keep track of the cursor position
+    var cursorPos = this.selectionStart,
+    // remove the dots (and any non-integer character), 
+    // split the string and reverse it
+        a = this.value.replace(/\D/g,'').split('').reverse();
+
+    // start from 3 and as long as there's a number 
+    // add a dot every three digits.
+    var pos = 3;
+    while(a[pos] !== undefined){
+      a.splice(pos,0,'.');
+      pos = pos + 4;
+    }  
+    // reverse, join and reassign the value
+    this.value = a.reverse().join(''); 
+  }
+
+  var Dotdot = function( node, opts ){
+
+    if(!node){
+      throw new Error('Dotdot needs an input element to be passed as a parameter');
+    }
 
     if(opts){
       DEFAULTS = _extend(DEFAULTS, opts);      
     }
 
-    el.addEventListener('keyup', function(){
-      // prevent every character except numbers to be entered
-      if(!this.value.slice(-1).match(/^[0-9]+\.?[0-9]*$/) ){
-        this.value = this.value.slice(0, -1);
-        return;
-      }
-      // remove the dots, split the string and reverse it
-      var a = this.value.replace(/\./g, '').split('').reverse();
-
-      // start from 3 and as long as there's a number 
-      // add a dot every three digits.
-      var pos = 3;
-      while(a[pos] !== undefined){
-        a.splice(pos,0,'.');
-        pos = pos + 4;
-      }  
-      // reverse, join and reassign the value
-      this.value = a.reverse().join('');  
-    }, false);
+    if(node.length !== undefined){
+      Array.prototype.forEach.call(node, _attachEvts);
+    } else {
+      _attachEvts(node);
+    }
   };
 
   window.Dotdot = Dotdot;
