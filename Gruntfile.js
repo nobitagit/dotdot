daa26e9d185dc60031c4d88be1d7ad1bd409e4d6
Nobita <aurelio.ogliari@gmail.com>
2014-11-17 15:53:46 +0000

    make grunt watch test folder

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function(grunt) {
     },
     watch: {
       test: {
-        files: ['src/**/*.js'],
+        files: ['src/**/*.js', 'test/**/*.spec.js'],
         tasks: ['jasmine']
       }
     },
