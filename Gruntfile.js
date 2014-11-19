40893468af413916198408e9cf0166b2b79c98a4
Nobita <aurelio.ogliari@gmail.com>
2014-11-19 10:13:18 +0000

    add uglify task

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,18 +17,33 @@ module.exports = function(grunt) {
     },
     jshint: {
       all: ['src/**/*.js']
-    }    
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %>.js - v<%= pkg.version %>\n' +
+          'built on <%= grunt.template.today("yyyy-mm-dd") %> \n' +
+          'repo: <%= pkg.repository.url %> \n' +
+          'author: <%= pkg.author %> \n' +
+          'license: <%= pkg.license %> */ \n'
+      },      
+      build:{
+        files: {
+          'dist/dotdot.min.js': ['src/dotdot.js']
+        }
+      }
+    }      
   });
 
   // Load the plugins.
   grunt.loadNpmTasks('grunt-contrib-jasmine');
   grunt.loadNpmTasks('grunt-contrib-watch'); 
-  grunt.loadNpmTasks('grunt-contrib-jshint');   
+  grunt.loadNpmTasks('grunt-contrib-jshint'); 
+  grunt.loadNpmTasks('grunt-contrib-uglify');    
 
   // Default task(s).
   grunt.registerTask('test-watch', ['watch:test']);
   grunt.registerTask('test', ['jasmine']);
-  grunt.registerTask('build', ['jasmine', 'jshint']);
+  grunt.registerTask('build', ['jasmine', 'jshint', 'uglify']);
   grunt.registerTask('default', []);
 
 };
